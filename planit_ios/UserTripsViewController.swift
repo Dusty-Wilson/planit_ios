192b5815260971fbac126002ab0fe5fe2c6c6ab7
Dusty Wilson <dwwilson21@gmail.com>
2014-06-14 21:58:13 -0500

    made first get request

--- planit_ios/UserTripsViewController.swift
+++ planit_ios/UserTripsViewController.swift
@@ -1,17 +1,62 @@
 import UIKit
 
-class UserTripsViewController: UIViewController {
+class UserTripsViewController: UIViewController, NSURLConnectionDelegate, UITableViewDelegate {
 
+    @lazy var data = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        startConnection()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startConnection(){
+        let urlPath: String = "http://secure-taiga-5848.herokuapp.com/trips/1.json"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        connection.start()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        self.data.appendData(data)
+    }
+    
+//        func btnLoad_Click(sender: UIButton){
+//            startConnection()
+//        }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        // throwing an error on the line below (can't figure out where the error message is)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        println(jsonResult)
+    }
+    
+}
+        
+        
+
+//        let emptyDictionary = [:]
+//    var session = NSURLSession.sharedSession()
+//        session.dataTaskWithRequest(url,
+//            completionHandler: {(data: NSData!,
+//                response: NSURLResponse!,
+//                error: NSError!) in
+//                println(data)
+//                println(response)
+//                println(error)
+//            }).resume()
+    
+        
     
 
     /*
@@ -24,4 +69,4 @@ class UserTripsViewController: UIViewController {
     }
     */
 
-}
+
